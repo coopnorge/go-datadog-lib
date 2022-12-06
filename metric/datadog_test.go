70353ef14f8e92b138ca1b0775a766ab90e7d1f2
Artjoms Nemiro <artjoms.nemiro@coop.no>
2022-12-06 10:28:48 +0100

    Fix files package naming that was violates golang style

--- metric/datadog_test.go
+++ metric/datadog_test.go
@@ -1,29 +1,29 @@
-package metrics
+package metric
 
 import (
-	"testing"
+    "testing"
 
-	"github.com/coopnorge/go-datadog-lib/config"
+    "github.com/coopnorge/go-datadog-lib/config"
 
-	"github.com/stretchr/testify/assert"
+    "github.com/stretchr/testify/assert"
 )
 
 func TestNewDatadogMetrics(t *testing.T) {
-	cfg := config.DatadogConfig{
-		Env:            "dev",
-		Service:        "unit-test",
-		ServiceVersion: "VUnit",
-	}
+    cfg := config.DatadogConfig{
+        Env:            "dev",
+        Service:        "unit-test",
+        ServiceVersion: "VUnit",
+    }
 
-	m := NewDatadogMetrics(&cfg)
+    m := NewDatadogMetrics(&cfg)
 
-	assert.True(t, m.defaultMetricsTags[0] == "environment:dev")
-	assert.True(t, m.defaultMetricsTags[1] == "service:unit-test")
-	assert.True(t, m.defaultMetricsTags[2] == "version:VUnit")
+    assert.True(t, m.defaultMetricsTags[0] == "environment:dev")
+    assert.True(t, m.defaultMetricsTags[1] == "service:unit-test")
+    assert.True(t, m.defaultMetricsTags[2] == "version:VUnit")
 
-	assert.True(t, m.servicePrefix == "unit_test")
+    assert.True(t, m.servicePrefix == "unit_test")
 
-	assert.Nil(t, m.GetClient())
-	assert.ElementsMatch(t, m.defaultMetricsTags, m.GetDefaultTags())
-	assert.Equal(t, m.GetServiceNamePrefix(), "unit_test")
+    assert.Nil(t, m.GetClient())
+    assert.ElementsMatch(t, m.defaultMetricsTags, m.GetDefaultTags())
+    assert.Equal(t, m.GetServiceNamePrefix(), "unit_test")
 }

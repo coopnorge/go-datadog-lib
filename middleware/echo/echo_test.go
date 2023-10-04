cba4477442937bc271bb29a7663b7d66e6d4e6b4
martinostvik <martin.ostvik@coop.no>
2023-10-04 10:20:23 +0200

    remove request is nil test as context.request always should return a request object

--- middleware/echo/echo_test.go
+++ middleware/echo/echo_test.go
@@ -5,8 +5,6 @@ import (
 	"testing"
 
 	mock_echo "github.com/coopnorge/go-datadog-lib/v2/internal/generated/mocks/labstack/echo/v4"
-	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
-
 	"github.com/golang/mock/gomock"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
@@ -35,28 +33,4 @@ func TestTraceServerMiddleware(t *testing.T) {
 	echoHandlerFunc := echoMiddlewareFun(mockEchoContext)
 
 	assert.Nil(t, echoHandlerFunc)
-}
-
-func TestTraceServerMiddlewareEchoMissingRequest(t *testing.T) {
-	echoMiddlewareHandler := TraceServerMiddleware()
-	echoRequestHandler := func(reqCtx echo.Context) (err error) {
-		assert.NotNil(t, reqCtx.Request())
-
-		span, exists := tracer.SpanFromContext(reqCtx.Request().Context())
-		assert.True(t, exists)
-		assert.NotNil(t, span)
-
-		return nil
-	}
-
-	ctrl := gomock.NewController(t)
-	mockEchoContext := mock_echo.NewMockContext(ctrl)
-	ctrl.Finish()
-
-	mockEchoContext.EXPECT().Request().Return(nil).MaxTimes(1)
-
-	echoMiddlewareFun := echoMiddlewareHandler(echoRequestHandler)
-	echoHandlerFunc := echoMiddlewareFun(mockEchoContext)
-
-	assert.NotNil(t, echoHandlerFunc)
 }

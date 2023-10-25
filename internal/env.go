0ed039ef0fc24a46b9fc8cec7ecd92f98808460b
Bendik Nesbø <bendik.august.nesbo@coop.no>
2023-10-25 10:57:16 +0200

    add wrapper for net/http clients

--- internal/env.go
+++ internal/env.go
@@ -5,10 +5,32 @@ import (
 	"strings"
 )
 
-// ExperimentalTracingEnabled is the environment variable key determining if experimental tracing should be enabled
-const ExperimentalTracingEnabled = "DD_EXPERIMENTAL_TRACING_ENABLED"
+const (
+	// ExperimentalTracingEnabled is the environment variable key determining if experimental tracing should be enabled.
+	ExperimentalTracingEnabled = "DD_EXPERIMENTAL_TRACING_ENABLED"
+	// DatadogEnvironment is the environment variable key determining the Datadog Environment to use.
+	DatadogEnvironment = "DD_ENV"
+	// DatadogService is the environment variable key for the name of the current service.
+	DatadogService = "DD_SERVICE"
+	// DatadogVersion is the environment variable key for the version of the current service.
+	DatadogVersion = "DD_VERSION"
+)
 
 // IsExperimentalTracingEnabled checks if experimental tracing is enabled
 func IsExperimentalTracingEnabled() bool {
 	return strings.ToLower(os.Getenv(ExperimentalTracingEnabled)) == "true"
+}
+
+// IsDatadogConfigured checks some common environment-variables to determine if the service is configured to use Datadog.
+func IsDatadogConfigured() bool {
+	if val := os.Getenv(DatadogEnvironment); val != "" {
+		return true
+	}
+	if val := os.Getenv(DatadogService); val != "" {
+		return true
+	}
+	if val := os.Getenv(DatadogVersion); val != "" {
+		return true
+	}
+	return false
 }

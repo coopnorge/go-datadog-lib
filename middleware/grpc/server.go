c9f32d72d75cf099036d73bfde98b5217ed55cfc
Bendik Nesbø <bendik.august.nesbo@coop.no>
2023-12-28 13:44:03 +0100

    feat(grpc): Add additional versions of grpc-interceptors, which are configurable
    Add options-pattern for configuring the grpc-server/client interceptors, and deprecate the old non-options interceptor.

--- middleware/grpc/server.go
+++ middleware/grpc/server.go
@@ -6,11 +6,25 @@ import (
 )
 
 // TraceUnaryServerInterceptor for Datadog Log Integration, middleware will create span that can be used from context
+// Deprecated: Use UnaryServerInterceptor instead. This function will be removed in a later version.
 func TraceUnaryServerInterceptor() grpc.UnaryServerInterceptor {
 	return ddGrpc.UnaryServerInterceptor()
 }
 
 // TraceStreamServerInterceptor for Datadog Log Integration, middleware will create span that can be used from context
+// Deprecated: Use StreamServerInterceptor instead. This function will be removed in a later version.
 func TraceStreamServerInterceptor() grpc.StreamServerInterceptor {
 	return ddGrpc.StreamServerInterceptor()
+}
+
+// UnaryServerInterceptor returns a middleware that creates datadog-spans on incoming requests, and stores them in the requests' context.
+func UnaryServerInterceptor(options ...Option) grpc.UnaryServerInterceptor {
+	opts := convertOptions(options...)
+	return ddGrpc.UnaryServerInterceptor(opts...)
+}
+
+// StreamServerInterceptor returns a middleware that creates datadog-spans on incoming requests, and stores them in the requests' context.
+func StreamServerInterceptor(options ...Option) grpc.StreamServerInterceptor {
+	opts := convertOptions(options...)
+	return ddGrpc.StreamServerInterceptor(opts...)
 }

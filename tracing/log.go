a4569f913251a7635fadd86cafa2e3c9b7789fa7
Niels Henrik Hagen <nhhagen@gmail.com>
2024-01-23 13:25:45 +0100

    chore: Deprecate funtions replaced by logger.WithContext (#260)
    * chore: Deprecate funtions replaced by logger.WithContext
    
    * chore: Deprecate functions and replace them with logger.WithContext (#261)
    
    ---------
    
    Co-authored-by: Bendik August Nesbø <bendik.august.nesbo@coop.no>

--- tracing/log.go
+++ tracing/log.go
@@ -2,52 +2,41 @@ package tracing
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/coopnorge/go-logger"
-	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
 // LogWithTrace will log message by logger.Level with trace if it's present in context.Context
+//
+// Deprecated: LogWithTrace will be removed in a future major version and
+// should not be used. Use logger.WithContext(ctx Context) from
+// github.com/coopnorge/go-logger
 func LogWithTrace(sourceCtx context.Context, severity logger.Level, message string) {
-	messageToLog := getMessageToLog(sourceCtx, message)
-	emptyEntry := logger.WithFields(map[string]interface{}{})
-
-	logWithSeverity(emptyEntry, severity, messageToLog)
+	entry := logger.WithContext(sourceCtx)
+	logWithSeverity(entry, severity, message)
 }
 
 // LogFieldsWithTrace will log message by logger.Level with trace if it's present in context.Context
+//
+// Deprecated: LogFieldsWithTrace will be removed in a future major version and
+// should not be used. Use logger.WithContext(ctx Context) from
+// github.com/coopnorge/go-logger
 func LogFieldsWithTrace(sourceCtx context.Context, severity logger.Level, message string, fields logger.Fields) {
-	messageToLog := getMessageToLog(sourceCtx, message)
-	entry := logger.WithFields(fields)
-
-	logWithSeverity(entry, severity, messageToLog)
+	entry := logger.WithContext(sourceCtx).WithFields(fields)
+	logWithSeverity(entry, severity, message)
 }
 
-func getMessageToLog(ctx context.Context, message string) string {
-	var messageToLog string
-
-	span, exists := tracer.SpanFromContext(ctx)
-	if exists {
-		messageToLog = fmt.Sprintf("%s %v dd.lang=go", message, span)
-	} else {
-		messageToLog = message
-	}
-
-	return messageToLog
-}
-
-func logWithSeverity(entry logger.Entry, severity logger.Level, message string) {
+func logWithSeverity(entry *logger.Entry, severity logger.Level, message string) {
 	switch severity {
 	case logger.LevelFatal:
-		entry.Fatalf(message)
+		entry.Fatal(message)
 	case logger.LevelError:
-		entry.Errorf(message)
+		entry.Error(message)
 	case logger.LevelWarn:
-		entry.Warnf(message)
+		entry.Warn(message)
 	case logger.LevelInfo:
-		entry.Infof(message)
+		entry.Info(message)
 	case logger.LevelDebug:
-		entry.Debugf(message)
+		entry.Debug(message)
 	}
 }

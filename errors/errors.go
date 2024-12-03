f6d7ce584bd27b74b273bdf96872d567cc371d34
Filip Stawicki <filip.stawicki@coop.no>
2024-12-03 14:05:47 +0100

    update ErrorHandler type

--- errors/errors.go
+++ errors/errors.go
@@ -1,5 +1,7 @@
 package errors
 
+import "context"
+
 // ErrorHandler allows for handling of error that cannot be returned to the
 // caller
-type ErrorHandler func(error)
+type ErrorHandler func(context.Context, error) error

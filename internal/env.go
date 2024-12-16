3c17e6b12c7820fab89db58b43f48880b5ee5f84
Niels Henrik Hagen <nhhagen@gmail.com>
2024-12-16 15:18:30 +0100

    feat: New bootstrap function (#430)
    feat: New bootstrap function
    
    Bootstrap function that checks for the required configuration and return a
    function to stop the Datadog integration.
    
    Closes: https://github.com/coopnorge/go-datadog-lib/issues/427

--- internal/env.go
+++ internal/env.go
@@ -1,11 +1,16 @@
 package internal
 
 import (
+	"fmt"
 	"os"
 	"strconv"
 )
 
 const (
+	// DatadogDisable is the environment variable key for whether to disable the Datadog integration.
+	DatadogDisable = "DD_DISABLE"
+	// DatadogEnableExtraProfiling is the environment variable key for whether to enable extra profiling or not.
+	DatadogEnableExtraProfiling = "DD_ENABLE_EXTRA_PROFILING"
 	// DatadogEnvironment is the environment variable key determining the Datadog Environment to use.
 	DatadogEnvironment = "DD_ENV"
 	// DatadogService is the environment variable key for the name of the current service.
@@ -16,20 +21,39 @@ const (
 	DatadogDSDEndpoint = "DD_DOGSTATSD_URL"
 	// DatadogAPMEndpoint is the environment variable key for the URL to APM.
 	DatadogAPMEndpoint = "DD_TRACE_AGENT_URL"
-	// DatadogEnableExtraProfiling is the environment variable key for whether to enable extra profiling or not.
-	DatadogEnableExtraProfiling = "DD_ENABLE_EXTRA_PROFILING"
-	// DatadogDisable is the environment variable key for whether to disable the Datadog integration.
-	DatadogDisable = "DD_DISABLE"
+	// DatadogAgentHost is the environment variable key for the host name to
+	// connect to Datadog.
+	DatadogAgentHost = "DD_AGENT_HOST"
 )
 
 // IsDatadogDisabled checks if the Datadog integration is disabled. The
 // environment variable DD_DISABLE is checked. If the variable is missing or
 // cannot be parsed to a bool the Datadog integration is assumed to be enabled.
 func IsDatadogDisabled() bool {
-	valStr := os.Getenv(DatadogDisable)
+	return GetBool(DatadogDisable, false)
+}
+
+// VerifyEnvVarsSet checks if the provided environmental variables are defined
+func VerifyEnvVarsSet(keys ...string) error {
+	for _, key := range keys {
+		val, ok := os.LookupEnv(key)
+		if !ok || val == "" {
+			return fmt.Errorf("required environmental variable not set: %q", key)
+		}
+	}
+	return nil
+}
+
+// GetBool returns the boolean value of the environmental variable, if the key
+// is not set or parsing fails the fallback value is returned.
+func GetBool(key string, fallback bool) bool {
+	valStr, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
 	val, err := strconv.ParseBool(valStr)
 	if err != nil {
-		return false
+		return fallback
 	}
 	return val
 }

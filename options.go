1797e3442c48144b583c94c52c88c3be865f866a
Niels Henrik Hagen <nhhagen@gmail.com>
2024-12-19 13:56:45 +0100

    feat: Add support for a stop timeout

--- options.go
+++ options.go
@@ -1,6 +1,8 @@
 package coopdatadog
 
 import (
+	"time"
+
 	"github.com/coopnorge/go-datadog-lib/v2/errors"
 	"github.com/coopnorge/go-datadog-lib/v2/internal"
 	"github.com/coopnorge/go-logger"
@@ -10,10 +12,17 @@ const (
 	defaultEnableExtraProfiling = false
 )
 
+var defaultStopTimeout = timeout(10 * time.Second)
+
+func timeout(duration time.Duration) *time.Duration {
+	return &duration
+}
+
 // options is the internal configuration for the Datadog integration
 type options struct {
 	enableExtraProfiling bool
 	errorHandler         errors.ErrorHandler
+	stopTimeout          *time.Duration
 }
 
 func resolveOptions(opts []Option) (*options, error) {
@@ -22,6 +31,7 @@ func resolveOptions(opts []Option) (*options, error) {
 		errorHandler: func(err error) {
 			logger.WithError(err).Error(err.Error())
 		},
+		stopTimeout: defaultStopTimeout,
 	}
 	opts = append([]Option{withConfigFromEnvVars()}, opts...)
 
@@ -49,6 +59,24 @@ func withConfigFromEnvVars() Option {
 func WithErrorHandler(handler errors.ErrorHandler) Option {
 	return func(options *options) error {
 		options.errorHandler = handler
+		return nil
+	}
+}
+
+// WithStopTimeout sets the allowed time for graceful shutdown, defaults to 10
+// seconds.
+func WithStopTimeout(timeout time.Duration) Option {
+	return func(options *options) error {
+		options.stopTimeout = &timeout
+		return nil
+	}
+}
+
+// WithNoStopTimeout waits for graceful shutdown to complete regardless of how
+// long.
+func WithNoStopTimeout() Option {
+	return func(options *options) error {
+		options.stopTimeout = nil
 		return nil
 	}
 }

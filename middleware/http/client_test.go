f15c3d2680c10a9d989c58129fa2381dfbe568b0
Bendik Nesbø <bendik.august.nesbo@coop.no>
2025-02-27 19:25:53 +0100

    fix: Do not log full URL in span-tags
    Closes https://github.com/coopnorge/go-datadog-lib/issues/495

--- middleware/http/client_test.go
+++ middleware/http/client_test.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strconv"
@@ -12,6 +13,7 @@ import (
 	"github.com/coopnorge/go-datadog-lib/v2/internal/testhelpers"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/ext"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/mocktracer"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
@@ -108,4 +110,47 @@ func TestWrapClientW3C(t *testing.T) {
 		}
 	}
 	assert.True(t, found, "Did not find Datadog's list-member in w3c tracestate")
+}
+
+func TestURLIsNotInTags(t *testing.T) {
+	// This test is a regression-test for this issue: https://github.com/coopnorge/go-datadog-lib/issues/495
+	testhelpers.ConfigureDatadog(t)
+
+	// Start Datadog tracer, so that we don't create NoopSpans.
+	testTracer := mocktracer.Start()
+
+	span, ctx := tracer.StartSpanFromContext(context.Background(), "http.request", tracer.ResourceName("/helloworld"))
+	defer span.Finish()
+
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		_, _ = w.Write([]byte("OK"))
+	}))
+
+	t.Cleanup(s.Close)
+
+	url := fmt.Sprintf("%s/some-path-with-pii?some-query-with-pii=true", s.URL)
+
+	// Adding tracing to client, with a static resource-name, as we want to make sure that no tags automatically add the full URL, which might contain PII (Personally Identifiable Information).
+	client := AddTracingToClient(&http.Client{Timeout: 500 * time.Millisecond}, WithResourceNamer(StaticResourceNamer("my-resource-name")))
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	require.NoError(t, err)
+
+	_, err = client.Do(req)
+	require.NoError(t, err)
+
+	testTracer.Stop()
+
+	spans := testTracer.FinishedSpans()
+	require.Equal(t, 1, len(spans))
+	finishedSpan := spans[0]
+	assert.Empty(t, finishedSpan.Tag(ext.HTTPURL))
+	assert.NotContains(t, finishedSpan.OperationName(), "pii")
+	assert.NotContains(t, finishedSpan.String(), "pii")
+	// Iterate over every tag, to make sure that none of the tags contain the full URL or the word 'pii' from the URL.
+	for tag, tagValue := range finishedSpan.Tags() {
+		if str, ok := tagValue.(string); ok {
+			assert.NotContains(t, str, "pii", "Tag %q contained the word 'pii', but it should not, as it is somehow picked up from the path or query! Full value: %s", tag, str)
+			assert.NotContains(t, str, url, "Tag %q contained the full URL, but it should not, as it might contain PII! Full value: %s", tag, str)
+		}
+	}
 }

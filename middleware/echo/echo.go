b8d023d1a2e206adc09ee298dfeb83d718cfbfce
Niels Henrik Sandaa Hagen <nhhagen@gmail.com>
2026-04-07 09:25:24 +0200

    Add echo Wrap function
    This change handles the deprecation of the Middleware function

--- middleware/echo/echo.go
+++ middleware/echo/echo.go
@@ -6,13 +6,30 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+// Wrap configures the provided [echo.Echo] and returns it. This is a
+// convenience function that calls [ddEcho.Wrap] with the provided [echo.Echo]
+// It is recommended to use this if you want to benefit from future tracer
+// features that require additional properties to be configured without having
+// to update your code.
+func Wrap(e *echo.Echo) *echo.Echo {
+	if internal.IsDatadogDisabled() {
+		return e
+	}
+
+	e = ddEcho.Wrap(e)
+	return e
+}
+
 // TraceServerMiddleware for Datadog Log Integration, middleware will create span that can be used from context
+//
+// Deprecated: Use of the [Wrap] function is recommended instead of directly calling
+// [echo.TraceServerMiddleware], as [Wrap] activates all available features automatically.
 func TraceServerMiddleware() echo.MiddlewareFunc {
 	if internal.IsDatadogDisabled() {
 		return noOpMiddlewareFunc()
 	}
 
-	return ddEcho.Middleware()
+	return ddEcho.Middleware() // nolint:staticcheck
 }
 
 func noOpMiddlewareFunc() echo.MiddlewareFunc {
